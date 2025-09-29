c3281a1e2f019c99d13823c14348013c00df2096
dev-usman04 <bighalo77@gmail.com>
2025-09-29 12:29:37 +0100

    update deployment

--- FUNDLI-Backend/src/server.js
+++ FUNDLI-Backend/src/server.js
@@ -64,7 +64,7 @@ app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? process.env.FRONTEND_URL 
-    : ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+    : ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://fundli-hjqn.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
